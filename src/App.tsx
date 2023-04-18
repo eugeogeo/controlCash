f78b59cbf29e3686fb7f3532163012e9a53525b4
Geovana <geovanaf1912@gmail.com>
2023-04-18 20:51:25 -0300

    feat: criando conexão com api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const test = async () => {
+		try{
+			// const response = await fetch('http://localhost:3334/transaction');
+			const response = await axios.get('http://localhost:3334/transaction');
+			console.log(response);
+		}catch{
+			console.log('erro');
+		}
+	};
+
+	useEffect(()=>{
+		test();
+	});
+	return (
+		<div>
+			<p>hello word</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
